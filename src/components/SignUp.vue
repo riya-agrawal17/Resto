67ecbb33ee0209cd65894f59e5d065ef20c58a56
Surya Prakash <surya@scrollstack.com>
2021-08-20 11:17:20 +0530

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,38 @@
+<template>
+  <img class="logo" src="../assets/logo.png" alt="" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Email" />
+    <input type="password" placeholder="Enter Password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SignUp',
+};
+</script>
+<style>
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid lightgreen;
+}
+.register button {
+  width: 320px;
+  height: 40px;
+  background: rgb(46, 63, 46);
+  border: 1px solid rgb(47, 77, 47);
+  color: white;
+  cursor: pointer;
+}
+</style>
